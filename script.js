dac170b4cde13ce91512ba9c13ffbf5718168618
richardjim <79172294+richardjim@users.noreply.github.com>
2021-08-12 17:56:01 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+$(document).ready(() => {
+  $("#searchForm").on("submit", (e) => {
+    let searchText = $("#searchText").val();
+    getPicture(searchText);
+    e.preventDefault();
+  });
+});
+
+function getPicture(searchText) {
+  axios.get("https://pixabay.com/api/?key=22894566-4e17b23e8144e49c421bbf7f0&q" + searchText)
+    .then((response) => {
+      console.log(response);
+      let images = response.data.Search;
+      let output = "";
+      $.each(images, (index, pics) => {
+        output += `
+        <div class= "col-md-3">
+        <div class= "text-center">
+        <img src="${pics.} />
+        </div>
+        
+        </div>
+        
+        `;
+      });
+
+      $("#pictures").html(output);
+    })
+
+    .catch(function (error) {
+      console.log(error);
+    });
+}
